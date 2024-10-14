d6e9c3fab7f4bce0fe7908a3fa91b543a70ec399
Hector <gt12930@GMAIL.com>
2024-10-14 06:21:12 -0500

    fix: asistencias

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
-    username: "usuario",
-    password: "S3gur0!Passw0rd",
-    database: "asistencias",
-    host: "3.145.205.44",
-    dialect: "mysql",
-  };
-  
+  username: "usuario",
+  password: "S3gur0!Passw0rd",
+  database: "asistencias",
+  host: "3.145.205.44",
+  dialect: "mysql",
+  timezone: "-05:00", // Especifica la zona horaria, por ejemplo, para Lima, Perú (-05:00)
+};
