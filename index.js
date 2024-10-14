0f3d35c927b135ddb852421d405a08f61c57e105
Hector <gt12930@GMAIL.com>
2024-10-14 06:18:52 -0500

    fix: asistencias

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const routerApi = require("./src/routes");
 const app = express();
-const port = 3009;
+const port = 3001;
 const path = require('node:path');
 
 app.use(express.json());
