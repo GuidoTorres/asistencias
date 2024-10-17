7c8d18b4a92421f8e82a7848d0a36e7a16e759c3
Hector <gt12930@GMAIL.com>
2024-10-16 20:00:42 -0500

    fix: db

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   username: "usuario",
   password: "S3gur0!Passw0rd",
   database: "asistencias",
-  host: "3.145.205.44",
+  host: "'asistenciasorsit.xyz'",
   dialect: "mysql",
   timezone: "-05:00", // Especifica la zona horaria, por ejemplo, para Lima, Perú (-05:00)
 };
